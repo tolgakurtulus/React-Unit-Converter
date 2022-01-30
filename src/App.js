aff7798e513494c67d839f039642f6fb38fd65ca
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-01-30 18:30:44 +0300

    add title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import convert from 'convert-units';
+import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Selector from './components/Selectors';
 import InputField from './components/InputField';
+
 
 class App extends React.Component {
     constructor() {
@@ -74,27 +77,35 @@ class App extends React.Component {
     render() {
         console.log(convert().possibilities())
         return (
-            <Container>
-                <Grid container justify='center' align='center' spacing={3}>
-                    <Selector data={{ size: 12, label: 'Measurement', measurements: convert().measures(), populateType: 'mainSelector', populateWith: this.generateMainSelector(), selectedValue: this.state.mainSelector }} handleSelector={this.handleMainSelector} />
-                    {
-                        this.state.mainSelector ? (
-                            <>
-                                <Selector data={{ size: 6, label: 'Quantity', populateType: 'firstQuantity', populateWith: convert().list(this.state.mainSelector), selectedValue: this.state.firstQuantity }} handleSelector={this.handleSelector} />
-                                <Selector data={{ size: 6, label: 'Quantity', populateType: 'secondQuantity', populateWith: convert().list(this.state.mainSelector), selectedValue: this.state.secondQuantity }} handleSelector={this.handleSelector} />
-                            </>
-                        ) :(<></>)
-                    }
-                    {
-                        this.state.firstQuantity && this.state.secondQuantity ? (
-                            <>
-                                <InputField data={{ currentValue: this.state.firstInput, input: this.state.firstQuantity, label: this.state.firstLabel, name: 'firstInput' }} handleInput={this.handleInputFields} />
-                                <InputField data={{ currentValue: this.state.secondInput, input: this.state.secondQuantity, label: this.state.secondLabel, name: 'secondInput' }} handleInput={this.handleInputFields} />
-                            </>
-                        ) : (<></>)
-                    }
-                </Grid>
-            </Container>
+            <>
+                <Box m={4}>
+                    <Typography variant="h3" component="h3" align="center" sx={{ m: "2rem" }}>
+                            Unit Converter
+                    </Typography>;
+                </Box>
+                <Container>
+                    <Grid container justify='center' align='center' spacing={3}>
+                        <Selector data={{ size: 12, label: 'Measurement', measurements: convert().measures(), populateType: 'mainSelector', populateWith: this.generateMainSelector(), selectedValue: this.state.mainSelector }} handleSelector={this.handleMainSelector} />
+                        {
+                            this.state.mainSelector ? (
+                                <>
+                                    <Selector data={{ size: 6, label: 'Quantity', populateType: 'firstQuantity', populateWith: convert().list(this.state.mainSelector), selectedValue: this.state.firstQuantity }} handleSelector={this.handleSelector} />
+                                    <Selector data={{ size: 6, label: 'Quantity', populateType: 'secondQuantity', populateWith: convert().list(this.state.mainSelector), selectedValue: this.state.secondQuantity }} handleSelector={this.handleSelector} />
+                                </>
+                            ) :(<></>)
+                        }
+                        {
+                            this.state.firstQuantity && this.state.secondQuantity ? (
+                                <>
+                                    <InputField data={{ currentValue: this.state.firstInput, input: this.state.firstQuantity, label: this.state.firstLabel, name: 'firstInput' }} handleInput={this.handleInputFields} />
+                                    <InputField data={{ currentValue: this.state.secondInput, input: this.state.secondQuantity, label: this.state.secondLabel, name: 'secondInput' }} handleInput={this.handleInputFields} />
+                                </>
+                            ) : (<></>)
+                        }
+                    </Grid>
+                </Container>
+            </>
+
         )
     }
 }
