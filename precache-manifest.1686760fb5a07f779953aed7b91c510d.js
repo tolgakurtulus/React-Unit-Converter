64f4f461271dad0ff30fa8455a958a112dda900f
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-01-30 18:19:37 +0300

    Updates

---
+++ precache-manifest.1686760fb5a07f779953aed7b91c510d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9889134a1a7fe046cfcc080624d7dc28",
+    "url": "/React-Unit-Converter/index.html"
+  },
+  {
+    "revision": "c1b2f062490f3a108e8c",
+    "url": "/React-Unit-Converter/static/js/2.5818bc46.chunk.js"
+  },
+  {
+    "revision": "792ebf8c5f45eb5c0736",
+    "url": "/React-Unit-Converter/static/js/main.8a350040.chunk.js"
+  },
+  {
+    "revision": "d5d7b6ccf9f140692653",
+    "url": "/React-Unit-Converter/static/js/runtime-main.d1bfb69e.js"
+  }
+]);
